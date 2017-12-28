13d824566ac30f2540ac40c6ae72f06b1aa74352
anton <antonkats@gmail.com>
2017-12-28 09:21:56 -0500

    last commit before going away about to get great questions to display

---
+++ model/src/main/scala/life/plenty/model/GreatQuestions.scala
@@ -0,0 +1,20 @@
+package life.plenty.model
+
+object GreatQuestions {
+
+  val orderedConstructors: List[Space ⇒ GreatQuestion] =
+    List(new Why(_), new When(_), new Where(_))
+
+  class Why(override val parent: Space) extends GreatQuestion {
+    override val title: String = "Why"
+  }
+
+  class When(override val parent: Space) extends GreatQuestion {
+    override val title: String = "When"
+  }
+
+  class Where(override val parent: Space) extends GreatQuestion {
+    override val title: String = "Where"
+  }
+
+}
