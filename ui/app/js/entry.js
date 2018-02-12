23ed18a23018c3c89ace1da206756f9b3748cfc7
anton <antonkats@gmail.com>
2018-02-12 12:12:12 -0500

    um... pouchdb might not be a go

--- ui/app/js/entry.js
+++ ui/app/js/entry.js
@@ -1,5 +1,12 @@
 // require("babel-polyfill");
 
+import PouchDB from 'pouchdb'
+import PouchDBAuthentication from 'pouchdb-authentication'
+
+// window.PouchDB = require('pouchdb');
+// PouchDB.plugin(require('pouchdb-authentication'));
+PouchDB.plugin(PouchDBAuthentication);
+window.PouchDB = PouchDB;
 window.Hashes = require('jshashes');
 // window.Gun = require('gun'); // in NodeJS
 window.Gun = require('../../../../gun/gun'); // in NodeJS
