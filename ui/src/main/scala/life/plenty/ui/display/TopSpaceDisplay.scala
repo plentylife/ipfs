3364bff3222db33587606615927e19b908108ada
anton <antonkats@gmail.com>
2018-02-14 13:37:51 -0500

    moved layout module

--- ui/src/main/scala/life/plenty/ui/display/TopSpaceDisplay.scala
+++ ui/src/main/scala/life/plenty/ui/display/TopSpaceDisplay.scala
@@ -7,7 +7,7 @@ import life.plenty.model.modifiers.OctopusModifier
 import life.plenty.model.octopi.{ContainerSpace, Members, Space}
 import life.plenty.model.octopi.definition.Hub
 import life.plenty.ui.console
-import life.plenty.ui.display.meta.{ChildDisplay, ModularDisplay, ModularDisplayTrait}
+import life.plenty.ui.display.meta.{ChildDisplay, LayoutModule, ModularDisplay, ModularDisplayTrait}
 import life.plenty.ui.model.{DisplayModel, UiContext}
 import life.plenty.ui.model.DisplayModel.{ActionDisplay, DisplayModule, ModuleOverride, SingleActionModuleDisplay, getSiblingModules}
 import org.scalajs.dom.raw.Node
@@ -55,60 +55,3 @@ class TopSpaceDisplay(override val withinOctopus: Space) extends LayoutModule[Sp
   }
 }
 
-trait LayoutModule[T <: Hub] extends DisplayModule[T] {
-
-  protected val siblingModules: Vars[DisplayModule[Hub]] = Vars()
-  protected val children: Vars[Hub] = Vars[Hub]()
-  protected var rxChildren: Obs = null
-
-  override def update(): Unit = {
-    val sms = getSiblingModules(this)
-    siblingModules.value.clear()
-    siblingModules.value.insertAll(0, sms)
-    if (rxChildren == null) {
-      rxChildren = getChildren.foreach { cs ⇒
-        children.value.clear()
-        children.value.insertAll(0, cs)
-        console.trace(s"child updated ${withinOctopus} $cs")
-      }
-    }
-    console.trace(s"layout display updating $this $withinOctopus $sms overrides ${this.cachedOverrides.value}")
-  }
-
-  private lazy val modifiers: List[OctopusModifier[Hub]] = {
-    console.trace(s"child meta getting modifiers from ${withinOctopus.modules}")
-    withinOctopus.getModules({ case m: OctopusModifier[Hub] ⇒ m })
-  }
-
-  private def getChildren: Rx[List[Hub]] = {
-    console.trace(s"getting children ${withinOctopus} ${withinOctopus.id}")
-    val childrenRx: Rx[List[Hub]] = withinOctopus.rx.getAll({ case Child(c: Hub) ⇒ c })
-    val ordered = modifiers.foldLeft(childrenRx)((cs, mod) ⇒ {
-      console.trace(s"applying modifiers ${withinOctopus}")
-      mod.applyRx(cs): Rx[List[Hub]]
-    })
-    ordered
-  }
-
-  @dom
-  protected def displayHubs(seq: BindingSeq[Hub]#WithFilter, cssClass: String)(implicit os: List[ModuleOverride])
-  :Binding[Node] = {
-    console.trace(s"Layout display list (hubs) $seq")
-    <div class={cssClass}>
-      {for (c <- seq) yield DisplayModel.display(c, os, Option(this)).bind}
-    </div>
-  }
-
-  @dom
-  protected def displayModules(seq: BindingSeq[DisplayModule[_]]#WithFilter, cssClass: String)
-                              (implicit os: List[ModuleOverride]) :Binding[Node] = {
-    console.trace(s"Layout display list (modules) $seq")
-    <div class={cssClass}>
-      {for (m <- seq) yield m.display(this, os) map {_.bind} getOrElse DisplayModel.nospan.bind}
-    </div>
-  }
-
-  protected def siblingOverrides: List[ModuleOverride] = getSiblingModules(this) flatMap (m ⇒ {
-    if (m.doDisplay()) m.overrides else Nil
-  })
-}
