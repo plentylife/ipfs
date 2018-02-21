73206a25ba5249a80431d450bd039add91bda7e0
anton <antonkats@gmail.com>
2018-02-21 14:29:06 -0500

    vote allowance are filtered by root user filter too

--- ui/src/main/scala/life/plenty/ui/filters/RootSpaceUserTransactionFilter.scala
+++ ui/src/main/scala/life/plenty/ui/filters/RootSpaceUserTransactionFilter.scala
@@ -4,7 +4,7 @@ import life.plenty.model
 import life.plenty.ui.console
 import life.plenty.model.connection._
 import life.plenty.model.modifiers.RxConnectionFilters
-import life.plenty.model.octopi.{Transaction, User}
+import life.plenty.model.octopi.{Transaction, User, VoteAllowance}
 import life.plenty.model.octopi.definition.{AtInstantiation, Hub}
 import life.plenty.model.utils.GraphUtils
 import life.plenty.ui.model.UiContext
@@ -28,13 +28,19 @@ class RootSpaceUserTransactionFilter(override val withinOctopus: User) extends R
     console.trace(s"filtering on root $rootId connection $con")
     con match {
         // don't forget vote allowance
-      case Child(t: Transaction) ⇒
-        t.getOnContribution() flatMap {onContribution ⇒
-          val crp = GraphUtils.getRootParentOrSelf(onContribution)(ctxOwner)
-          console.trace(s"transaction parent root [$rootId] $crp ${crp.now.id} ${crp.now.id == rootId}")
-          if (crp().id == rootId) Option(con) else None
-        }
+      case Child(t: Transaction) ⇒ filterTransaction(rootId, t, ctxOwner) map (_ ⇒ con)
+      case Child(va: VoteAllowance) ⇒ va.onTransaction() flatMap {t ⇒
+        filterTransaction(rootId, t, ctxOwner)} map {_ ⇒ con}
       case _ ⇒ Option(con)
+    }
+  }
+
+  private def filterTransaction(rootId: String, t: Transaction, ctxOwner: Ctx.Owner)
+                               (implicit ctx: Ctx.Data): Option[Unit] = {
+    t.getOnContribution() flatMap {onContribution ⇒
+      val crp = GraphUtils.getRootParentOrSelf(onContribution)(ctxOwner)
+      console.trace(s"transaction parent root [$rootId] $crp ${crp.now.id} ${crp.now.id == rootId}")
+      if (crp().id == rootId) Option(Unit) else None
     }
   }
 }
