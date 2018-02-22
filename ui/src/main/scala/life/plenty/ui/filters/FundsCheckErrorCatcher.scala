36ab45be6af8eb6cee9a2529a0905a1dff448fca
anton <antonkats@gmail.com>
2018-02-22 08:24:29 -0500

    error modal style improvements

--- ui/src/main/scala/life/plenty/ui/filters/FundsCheckErrorCatcher.scala
+++ ui/src/main/scala/life/plenty/ui/filters/FundsCheckErrorCatcher.scala
@@ -5,7 +5,7 @@ import life.plenty.model.actions.ActionCatchGraphTransformError
 import life.plenty.model.octopi.definition.Hub
 import life.plenty.model.permissions.{FundsError, NotEnoughThanks, NotEnoughVotingPower}
 import life.plenty.ui
-import life.plenty.ui.display.Modal
+import life.plenty.ui.display.{ErrorModal, Modal}
 import life.plenty.ui.model.UiContext
 import org.scalajs.dom.Node
 
@@ -14,14 +14,15 @@ class FundsCheckErrorCatcher(override val withinOctopus: Hub) extends ActionCatc
     e match {
       case e: NotEnoughThanks ⇒
         ui.console.error(e)
-        Modal.setContentAndOpen(notEnoughThanks(e), "error", "close")
+        if (UiContext.getUser.id == e.user.id) ErrorModal.setContentAndOpen(notEnoughThanks(e))
       case e: NotEnoughVotingPower ⇒
         ui.console.error(e)
-        Modal.setContentAndOpen(notEnoughVotes(e), "error", "close")
+        if (UiContext.getUser.id == e.user.id) ErrorModal.setContentAndOpen(notEnoughVotes(e))
       case _ ⇒ Unit
     }
   }
 
+  @deprecated
   private def address(e: FundsError) =
     if (UiContext.getUser.id == e.user.id) "You do" else s"${e.user.getNameOrEmpty.now} does"
 
@@ -29,14 +30,14 @@ class FundsCheckErrorCatcher(override val withinOctopus: Hub) extends ActionCatc
   @dom
   private def notEnoughThanks(e: NotEnoughThanks): Binding[Node] = {
     <span>{address(e)} not have enough {ui.thanks}hanks<br/>
-      You have to earn some by receiving {ui.thanks}hanks from the fellow members in the space for contributing
+      You have to earn some by receiving {ui.thanks}hanks from your fellow members in the space for contributing
     </span>
   }
 
   @dom
   private def notEnoughVotes(e: NotEnoughVotingPower): Binding[Node] = {
     <span>{address(e)} do not have enough voting power<br/>
-      You have to earn votes by spending {ui.thanks}thanks
+      You have to earn votes by spending {ui.thanks}thanks. Give some to your fellow members for contributing!
     </span>
   }
 }
