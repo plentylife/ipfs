77b4cf28b55a122dcd976936ace89d7e73a4d59b
anton <antonkats@gmail.com>
2018-02-26 14:16:19 -0500

    earnings bar. done

--- ui/src/main/scala/life/plenty/ui/display/MembersCardDisplay.scala
+++ ui/src/main/scala/life/plenty/ui/display/MembersCardDisplay.scala
@@ -15,7 +15,7 @@ import life.plenty.ui.display.utils.Helpers.{BasicBindable, BindableModule, OptB
 import life.plenty.ui.model.{ComplexModuleOverride, DisplayModel, ModuleOverride, UiContext}
 import org.scalajs.dom.Event
 import org.scalajs.dom.raw.Node
-import rx.{Ctx, Obs, Rx}
+import rx.{Ctx, Obs, Rx, Var ⇒ rxVar}
 import life.plenty.ui.display.utils.Helpers._
 
 import scalaz.std.list._
@@ -53,36 +53,55 @@ class MembersCardDisplay(override val hub: Members) extends DisplayModule[Member
     case Some(p: Space) ⇒ val l = GraphUtils.getAllContributionsInSpace(p); l()
     case _ ⇒ List()
   }}
-  private lazy val maxEarned = Var(0)
+  private lazy val maxEarned: rxVar[Int] = rxVar(0)
 
   private def displayMember(u: User): Binding[Node] = {
     val userContributions = Rx {
       println(s"contributions $contributions")
       contributions().filter(_.getCreator().exists(_.id == u.id))
     }
-    val b = new BadgeMemberEarned(u, userContributions, this)
+    val userEarned = userContributions.map(list ⇒ {
+      (0 :: list.map(_.tips())).sum
+    })
+    userEarned.foreach {ue ⇒ if (ue > maxEarned.now) maxEarned() = ue}
+    val b = new BadgeMemberEarned(u, userContributions, userEarned = userEarned, maxEarned, this)
+
+    userEarned.foreach(ue ⇒ println("max", maxEarned.now))
 
     b.html()
   }
 }
 
 
-class BadgeMemberEarned(val user: User, contributions: Rx[List[Contribution]], caller: DisplayModule[Hub])
+class BadgeMemberEarned(val user: User, contributions: Rx[List[Contribution]],
+                        userEarned: Rx[Int], maxEarned: Rx[Int],
+                        caller: DisplayModule[Hub])
                        (implicit ctx: Ctx.Owner) {
-  private implicit def parser(i: Int): String = s"$i contributions"
+  private implicit def parser(i: Int): String = i.toString
 
   private lazy val badgeDisplay = new BindableModule(user.getTopModule({case m: FullUserBadge ⇒ m}), caller)
   private lazy val contributionsCount = contributions.map(_.size)
+  private lazy val barWidth: BasicBindable[Double] = Rx {
+    if (maxEarned() == 0) 0 else userEarned().toDouble / maxEarned()
+  }
 
   @dom
   def html(): Binding[Node] = {
-    <div class="d-flex user-earned-box">
+    val widthStyle = s"width: ${barWidth().bind * 100}%"
+
+    <div class="d-flex user-space-contribution-box">
+      <div class="user-box">
+      {badgeDisplay.dom.bind}
+        <div class="badge badge-secondary">
+          {contributionsCount.dom.bind} contributions
+        </div>
+      </div>
+
       <div class="user-earned-bar-box">
-      {badgeDisplay.dom.bind} earned
+        {new BindableProperty(userEarned)(_.toString + ui.thanks).dom.bind}
+        <div class="user-earned-bar" style={widthStyle}></div>
       </div>
-      <div>
-        {contributionsCount.dom.bind}
-      </div>
+
     </div>
   }
 }
