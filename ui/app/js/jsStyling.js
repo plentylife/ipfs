cafee3f7b91635b42421f5d9d2fb745bc2941ae8
anton <antonkats@gmail.com>
2018-03-02 12:55:23 -0500

    scrolling works! i overcomplicated it. try again.

--- ui/app/js/jsStyling.js
+++ ui/app/js/jsStyling.js
@@ -12,10 +12,12 @@ function addScroll() {
   let atSection = 0;
   let allSections = [];
 
+  // going to assume that this will only be called when dom elements are removed
   window.addScrollToSectionContainer = function () {
     console.log("+++");
 
     let ss = document.getElementsByClassName("scroll-wrapper");
+    window.ss = ss
     let btns = document.getElementsByClassName("section-nav-buttons");
     if (ss.length > 0) {
       let s = ss[0];
@@ -23,18 +25,13 @@ function addScroll() {
       allSections = s.getElementsByClassName("section");
       console.log("sections", allSections, btns);
 
-      if (!tracking) {
-        attachSensors(s)
-      } else if (tracking !== s) {
-        detachSensors(s)
-      }
+      detachSensors(s)
 
-      if (!btnLeft) {
-        btnLeft = btns[0];
-        btnRight = btns[1];
-        btnLeft.addEventListener("click", function () {nav(false)});
-        btnRight.addEventListener("click", function () {nav(true)})
-      }
+      btnLeft = btns[0];
+      btnRight = btns[1];
+
+      attachSensors(s)
+      setNavButtonStatus()
     }
   };
 
@@ -53,28 +50,51 @@ function addScroll() {
     if (!sections) {
       sections = filterSections()
     }
-    let rightEnd = atSection === sections.length - 1
+
+    let rightEdgeOffScreen = true
+    // let leftEdgeOffScreen = true
+    if (sections.length === 0) {
+      rightEdgeOffScreen = false
+      // let leftEdgeOffScreen = false
+    } else {
+      let rightMost = sections[sections.length - 1]
+      let padding = window.getComputedStyle(rightMost, null).getPropertyValue('padding-right').replace("px", "");
+      padding = parseFloat(padding)
+      padding = 0
+      console.log("padding", padding)
+      rightEdgeOffScreen = rightMost.getBoundingClientRect().right + padding - window.innerWidth > 0
+      console.log("reos", sections[sections.length - 1], sections[sections.length - 1].getBoundingClientRect().right,
+        window.innerWidth, rightEdgeOffScreen)
+    }
+
+    // let leftEdgeOffScreen = sections[atSection].getBoundingClientRect().left - window.innerWidth > 0
+    // console.log("leos", sections[atSection].getBoundingClientRect().left, leftEdgeOffScreen)
+
+    let rightEnd = atSection >= sections.length - 1 && !rightEdgeOffScreen
     let leftEnd = atSection === 0
     if (rightEnd) {
       btnRight.classList.add("disabled")
-      btnLeft.classList.remove("disabled")
     }
     if (leftEnd) {
       btnLeft.classList.add("disabled")
-      btnRight.classList.remove("disabled")
     }
-    if (!(leftEnd || rightEnd)) {
+    if (!leftEnd) {
       btnLeft.classList.remove("disabled")
+    }
+    if (!rightEnd) {
       btnRight.classList.remove("disabled")
     }
+    console.log("btn status", leftEnd, rightEnd, sections, atSection)
   }
 
   function nav(right) {
+    console.log("nav")
     let sections = filterSections()
 
     let nextSection = atSection;
     if (right) {
-      if (nextSection < sections.length - 1) {
+      rightEdgeOffScreen = sections[sections.length - 1].getBoundingClientRect().right - window.innerWidth > 0
+      if (nextSection < sections.length - 1 && rightEdgeOffScreen) {
         nextSection++
       } else {
         return
@@ -87,21 +107,27 @@ function addScroll() {
       }
     }
     atSection = nextSection
-    setNavButtonStatus(sections)
 
     let s = sections[nextSection];
     console.log("scrolling to", s, nextSection);
     console.log("scrolling to", sections, sections.length);
     scroller.scrollToElement(s, 0, true, true)
+    setNavButtonStatus(sections)
   }
 
   function detachSensors() {
-    rs.detach();
-    scroller.destroy()
+    console.log("detaching sensors", rs, btnLeft, scroller)
+    if (rs) {
+      rs.detach();
+    }
+    // scroller.destroy()
   }
 
   function attachSensors(s) {
-    console.log("attaching to", s);
+    console.log("attaching sensors to", s);
+
+    btnLeft.addEventListener("click", function () {nav(false)});
+    btnRight.addEventListener("click", function () {nav(true)})
     tracking = s;
     scroller = new IScroll(s, {
       mouseWheel: true,
@@ -109,6 +135,10 @@ function addScroll() {
       freeScroll: true,
       scrollX: true,
     });
+    scroller.on("scrollEnd", function () {
+      console.log("finished scrolling")
+      setNavButtonStatus()
+    })
     rs = new ResizeSensor(s, modifyScroller);
     console.log("scroller", scroller)
   }
