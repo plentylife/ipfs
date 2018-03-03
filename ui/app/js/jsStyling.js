78fd1f17df616649f70147d1d34dc10ea11dbef3
anton <antonkats@gmail.com>
2018-03-02 19:39:11 -0500

    last go at scroll

--- ui/app/js/jsStyling.js
+++ ui/app/js/jsStyling.js
@@ -9,53 +9,61 @@ function addScroll() {
 
   // going to assume that this will only be called when dom elements are removed
   window.addScrollToSectionContainer = function () {
+    console.log("**** adding scroller")
     let sws = document.getElementsByClassName("scroll-wrapper");
     if (sws.length > 0) {
       scrollWrapper = sws[0];
       detachSensors(scrollWrapper)
       attachSensors(scrollWrapper)
+      modifyScroller()
     }
   };
 
-  let lastHeight = 0;
+  let lastHeight = "0px";
   function detachSensors() {
-    lastHeight = 0;
+    lastHeight = "0px";
     if (rs) {
       rs.detach();
     }
-    // scroller.destroy()
   }
 
   function attachSensors(s) {
     scroller = new IScroll(s, {
-      mouseWheel: true,
+      mouseWheel: false,
       scrollbars: true,
-      // freeScroll: true,
       scrollX: true,
       scrollY: false,
       eventPassthrough: true,
-      // zoom: false,
-      // wheelAction: 'zoom',
-      // zoomMin: 0.5,
-      // zoomMax: 2,
-      // startZoom: 0.7
+      interactiveScrollbars: true
     });
+    scroller.on("scrollStart", scrollStart)
+    scroller.on("scrollEnd", scrollEnd)
     rs = new ResizeSensor(s, modifyScroller);
   }
 
+  function scrollStart() {
+    scrollWrapper.classList.add("scrolling")
+    console.log("scroll stat")
+  }
+
+  function scrollEnd() {
+    scrollWrapper.classList.remove("scrolling")
+    console.log("scroll end")
+  }
+
   function modifyScroller() {
-    console.log("scroller refresh", scroller)
-    // console.log("refresh sensor")
-    scroller.refresh();
     if (scrollWrapper) {
       let content = scrollWrapper.getElementsByClassName("top-space-child-display")[0]
       let contentHeight = window.getComputedStyle(content)["height"]
       if (lastHeight != contentHeight) {
+        console.log(scrollWrapper.style)
         scrollWrapper.style.height = contentHeight
         console.log("scroller height set", contentHeight, lastHeight)
         lastHeight = contentHeight
       }
     }
+    scroller.refresh();
+    console.log("scroller refresh", scroller)
   }
 }
 
