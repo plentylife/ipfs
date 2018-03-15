5162e1466de3b52aed8cd6524e21031a7c60a520
anton <antonkats@gmail.com>
2018-03-15 07:21:20 -0400

    gitignore for scalaide

---
+++ model/src/main/scala/life/plenty/model/hub/pseudo/StrongHub.scala
@@ -0,0 +1,7 @@
+package life.plenty.model.hub.pseudo
+
+trait StrongHub
+
+trait CreatedProperty extends StrongHub {
+  val created: Long
+}
--- model/src/main/scala/life/plenty/model/hub/pseudo/StrongProperties.scala
+++
@@ -1,7 +0,0 @@
-package life.plenty.model.hub.pseudo
-
-trait StrongProperties
-
-trait CreatedProperty extends StrongProperties {
-  val created: Long
-}
