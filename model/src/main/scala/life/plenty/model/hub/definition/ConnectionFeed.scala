29f6e202b35228c5419cf6a4e44b9800e46e9801
anton <antonkats@gmail.com>
2018-03-20 18:15:19 -0400

    ok. maybe this is it.

--- model/src/main/scala/life/plenty/model/hub/definition/ConnectionFeed.scala
+++ model/src/main/scala/life/plenty/model/hub/definition/ConnectionFeed.scala
@@ -1,32 +1,34 @@
 package life.plenty.model.hub.definition
 
 import life.plenty.model.connection.{Active, DataHub, Inactive}
-import monix.execution.CancelableFuture
-import monix.reactive.{MulticastStrategy, Observable}
-import monix.reactive.OverflowStrategy.Unbounded
-import monix.reactive.observers.Subscriber
-import monix.execution.cancelables.SingleAssignCancelable
 import monix.execution.Scheduler.Implicits.global
-
-import scala.concurrent.Future
+import monix.reactive.{MulticastStrategy, Observable}
 
 sealed trait GraphOp[+T] {
   val value: T
   private[GraphOp] var _produced: Set[GraphOp[_]] = Set()
+
   def produce[P](graphOp: GraphOp[P]): GraphOp[P] = {
     _produced += graphOp
     graphOp
-    }
+  }
+
   def produced = _produced
+  def clear() = {
+    val p = _produced
+    _produced = Set()
+    p
+  }
 }
 
 object GraphOp {
+
   implicit class GraphOps[T](op: GraphOp[T]) {
     def collect[R](f: PartialFunction[T, R]): Option[GraphOp[R]] = {
-      Option(op.value).collect(f) map {v ⇒
+      Option(op.value).collect(f) map { v ⇒
         op match {
-          case i @ Insert(_) ⇒ i.produce(Insert(v))
-          case r @ Remove(_) ⇒ r.produce(Remove(v))
+          case i@Insert(_) ⇒ i.produce(Insert(v))
+          case r@Remove(_) ⇒ r.produce(Remove(v))
         }
       }
     }
@@ -34,13 +36,25 @@ object GraphOp {
 
   implicit class GraphOpsStream[T](stream: Observable[GraphOp[T]]) {
     def collectOps[R](f: PartialFunction[T, R]): Observable[GraphOp[R]] =
-      stream.map(_.collect(f)).collect({case Some(op) ⇒ op})
+      stream.map(_.collect(f)).collect({ case Some(op) ⇒ op })
 
     def depMap[M](mapFunction: T ⇒ Observable[M]): Observable[GraphOp[M]] = {
-      val branches = stream.filter(_.isInstanceOf[Insert]).map { op ⇒
-        val depObs = mapFunction(op.value)
-        val in = depObs.map(dep ⇒ op.produce(Insert(dep)))
+      var lastInserts = Map[T, Observable[List[M]]]()
+
+      val branches = stream.map {
+        case op@Insert(elem) ⇒
+          val depObs = mapFunction(op.value)
+          val in = depObs.map(dep ⇒ op.produce(Insert(dep)))
+          lastInserts += op.value → in.scan(List[M]()){(list, sop) ⇒
+            sop.value :: list
+          }
+          in
+        case op@Remove(elem) ⇒
+          lastInserts.get(elem).map(ins ⇒ {
+            ins.lastF.flatMap(list ⇒ Observable.fromIterable(list map {in ⇒ Remove(in)}))
+          }) getOrElse Observable.empty[GraphOp[M]]
       }
+
 
       val single: Observable[GraphOp[M]] = branches.flatten
       println("FLATMAP")
@@ -58,55 +72,51 @@ object GraphOp {
       case Insert(_) ⇒ true
     })
   }
-
-  def collectLeafDependants(op: GraphOp[_]): Set[GraphOp[_]] = {
-    val leaf = op.produced filter {_.produced.isEmpty}
-    val node = op.produced flatMap  {_.produced} flatMap collectLeafDependants
-    leaf ++ node
-  }
+//
+//  def collectLeafDependants(op: GraphOp[_]): Set[GraphOp[_]] = {
+//    val leaf = op.produced filter {_.produced.isEmpty}
+//    val node = op.produced flatMap {_.produced} flatMap collectLeafDependants
+//    leaf ++ node
+//  }
 }
 
 case class Insert[+T](value: T) extends GraphOp[T]
+
 case class Remove[+T](value: T) extends GraphOp[T]
 
 
-trait ConnectionFeed {self: ConnectionManager ⇒
+trait ConnectionFeed {
+  self: ConnectionManager ⇒
   val (feedSub, feed) = Observable.multicast[GraphOp[DataHub[_]]](MulticastStrategy.publish)
 
   def onInsert(con: DataHub[_]): Unit = {
-    var lastInsert: Option[Insert[DataHub[_]]] = None
-
     if (con.isActive) {
       val in = Insert(con)
-      lastInsert = Option(in)
       feedSub.onNext(in)
     }
-    con.isRemoved.foreach {r ⇒
-      if (r) {
-        lastInsert foreach {in ⇒
-          val leafs = GraphOp.collectLeafDependants(in)
-          val removes = leafs map {l ⇒ Remove(l.value)}
-          feedSub.feed(removes.toIterable)
-        }
+    con.isRemoved.foreach { r ⇒
+      val op = if (r) {
+        Remove(con)
       } else {
-        val in = Insert(con)
-        lastInsert = Option(in)
-        feedSub.onNext(in)
+        Insert(con)
       }
+      feedSub.onNext(op)
 
     }
   }
 
   def getStream: Observable[GraphOp[DataHub[_]]] = {
-    val existing: List[GraphOp[DataHub[_]]] = connections map {h ⇒ Insert(h : DataHub[_])}
+    val existing: List[GraphOp[DataHub[_]]] = connections map { h ⇒ Insert(h: DataHub[_]) }
     val existingObs: Observable[GraphOp[DataHub[_]]] = Observable.fromIterable(existing)
     existingObs ++ feed
   }
+
   def getStream[T](extractor: PartialFunction[DataHub[_], T]): Observable[GraphOp[T]] = {
-    getStream.map(_.collect(extractor)).collect({case Some(op) ⇒ op})
+    getStream.map(_.collect(extractor)).collect({ case Some(op) ⇒ op })
   }
+
   def getInsertStream: Observable[DataHub[_]] = {
-    Observable.fromIterable(connections) ++ feed.collect({case Insert(h) ⇒ h})
+    Observable.fromIterable(connections) ++ feed.collect({ case Insert(h) ⇒ h })
   }
 
   val isRemoved = feed.collect {
